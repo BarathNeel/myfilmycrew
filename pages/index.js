15bc98b35ffc2057ca14f09373d9db2890c2034b
BarathNeel <223136381+BarathNeel@users.noreply.github.com>
2025-07-29 11:53:23 +0530

    index page change

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>🎬 Welcome to MyFilmyCrew</h1>
-      <p>Find and hire the perfect film crew members with ease.</p>
+      <p>Find the perfect film crew members with ease.</p>
     </main>
   );
 }
